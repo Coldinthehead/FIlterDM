a5ba0878450429fc05a3f3932c812a6c42e2f1c8
coldinthehead <kroters1050595@gmail.com>
2025-01-18 14:23:47 +0300

    add open block test

--- FilterDM.Tests/ViewModel.Tests/EditorPanelViewModelTests.cs
+++ FilterDM.Tests/ViewModel.Tests/EditorPanelViewModelTests.cs
@@ -13,4 +13,15 @@ public class EditorPanelViewModelTests
 
         Assert.That(sut.Items.Count, Is.EqualTo(0));
     }
+    [Test]
+    public void AddBlock_ShouldCreateBlockEditor_WhenEditorNotExists()
+    {
+        EditorPanelViewModel sut = new();
+        BlockDetailsViewModel testBlock = new BlockDetailsViewModel(new(), new(), new(new()));
+
+        sut.AddBlock(testBlock);
+
+        Assert.That(sut.Items, Has.Count.EqualTo(1));
+        Assert.That(sut.Items.First().IsPartOf(testBlock), Is.True);
+    }
 }
