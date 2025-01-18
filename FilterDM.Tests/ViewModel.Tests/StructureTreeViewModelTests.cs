b866c1fd27722091d39f7306e04b94a68fc20874
coldinthehead <kroters1050595@gmail.com>
2025-01-18 14:08:00 +0300

    add structure tree tests

---
+++ FilterDM.Tests/ViewModel.Tests/StructureTreeViewModelTests.cs
@@ -0,0 +1,61 @@
+﻿using FilterDM.ViewModels;
+using FilterDM.ViewModels.EditPage;
+
+namespace FilterDM.Tests.ViewModel.Tests;
+public class StructureTreeViewModelTests
+{
+    private FilterViewModel _filterVm;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _filterVm = new(new(), new());
+        _filterVm.NewBlock();
+        _filterVm.NewBlock();
+    }
+
+
+    [Test]
+    public void SetBlocks_ShouldReplicateBlocks()
+    {
+        StructureTreeViewModel sut = new();
+
+        sut.SetBlocks(_filterVm.Blocks);
+
+        Assert.That(sut.Blocks, Is.EqualTo(_filterVm.Blocks));
+    }
+
+    [Test]
+    public void ShouldAddBlock_WhenNewBlockCreated()
+    {
+        StructureTreeViewModel sut = new();
+        sut.SetBlocks(_filterVm.Blocks);
+
+        _filterVm.NewBlock();
+
+        Assert.That(sut.Blocks, Has.Count.EqualTo(_filterVm.Blocks.Count));
+    }
+
+    [Test]
+    public void ShouldSelectLastCreatedBlock()
+    {
+        StructureTreeViewModel sut = new();
+        sut.SetBlocks(_filterVm.Blocks);
+
+        _filterVm.NewBlock();
+
+        Assert.That(sut.SelectedNode, Is.EqualTo(_filterVm.Blocks.Last()));
+    }
+
+    [Test]
+    public void ShouldClearSelection_WhenSelectedBlockDeleted()
+    {
+        StructureTreeViewModel sut = new();
+        sut.SetBlocks(_filterVm.Blocks);
+        sut.Select(_filterVm.Blocks.First());
+
+        _filterVm.DeleteBlock(_filterVm.Blocks.First());
+
+        Assert.That(sut.SelectedNode, Is.Null);
+    }
+}
