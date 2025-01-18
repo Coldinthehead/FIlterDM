f4a07d2ddd8a8e85f81e46d50d3964fb5266684d
coldinthehead <kroters1050595@gmail.com>
2025-01-18 14:20:21 +0300

    add clear test

--- FilterDM.Tests/ViewModel.Tests/EditorPanelViewModelTests.cs
+++ FilterDM.Tests/ViewModel.Tests/EditorPanelViewModelTests.cs
@@ -1,4 +1,16 @@
-﻿namespace FilterDM.Tests.ViewModel.Tests;
+﻿using FilterDM.ViewModels.EditPage;
+
+namespace FilterDM.Tests.ViewModel.Tests;
 public class EditorPanelViewModelTests
 {
+    [Test]
+    public void Clear_ShouldDeleteAllEditors()
+    {
+        EditorPanelViewModel sut = new();
+        sut.AddBlock(new BlockDetailsViewModel(new(), new(), new(new())));
+
+        sut.Clear();
+
+        Assert.That(sut.Items.Count, Is.EqualTo(0));
+    }
 }
