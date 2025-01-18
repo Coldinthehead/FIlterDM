7b34afdbb317489862d844e1b03c072d2b806bb4
coldinthehead <kroters1050595@gmail.com>
2025-01-18 11:28:36 +0300

    add new block test

---
+++ FilterDM.Tests/ViewModel.Tests/FilterViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿
+using FilterDM.ViewModels;
+
+namespace FilterDM.Tests.ViewModel.Tests;
+public class FilterViewModelTests
+{
+    [Test]
+    public void NewBlock_ShouldAddEmptyBlockToBlocks()
+    {
+        FilterViewModel sut = new FilterViewModel();
+
+        sut.NewBlock();
+
+        Assert.That(sut.Blocks, Has.Count.EqualTo(1));
+    }
+
+}
