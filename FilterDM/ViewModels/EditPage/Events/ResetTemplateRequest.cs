eb8bfef86afd751b086948660e21a87468740aa6
coldinthehead <kroters1050595@gmail.com>
2025-01-19 14:23:17 +0300

    add reset tempalte request event

---
+++ FilterDM/ViewModels/EditPage/Events/ResetTemplateRequest.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+
+namespace FilterDM.ViewModels.EditPage.Events;
+
+public class ResetTemplateRequest : ValueChangedMessage<TemplateChangeDetils>
+{
+    public ResetTemplateRequest(TemplateChangeDetils value) : base(value)
+    {
+    }
+}
+
---
+++ FilterDM/ViewModels/EditPage/Events/TemplateChangeDetils.cs
@@ -0,0 +1,14 @@
+﻿namespace FilterDM.ViewModels.EditPage.Events;
+
+public struct TemplateChangeDetils
+{
+    public readonly BlockDetailsViewModel Block;
+    public readonly string TempalteName;
+
+    public TemplateChangeDetils(BlockDetailsViewModel block, string tempalteName)
+    {
+        Block = block;
+        TempalteName = tempalteName;
+    }
+}
+
