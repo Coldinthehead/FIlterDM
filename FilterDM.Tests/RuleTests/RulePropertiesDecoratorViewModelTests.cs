f232d994c4621437539190e2e2c7e7a93bd48d09
coldinthehead <kroters1050595@gmail.com>
2025-01-19 16:41:01 +0300

    add rule on sort test

---
+++ FilterDM.Tests/RuleTests/RulePropertiesDecoratorViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using FilterDM.Tests.ViewModel.Tests;
+using FilterDM.ViewModels.EditPage;
+using FilterDM.ViewModels.EditPage.Decorators;
+using FilterDM.ViewModels.EditPage.Events;
+
+namespace FilterDM.Tests.RuleTests;
+public class RulePropertiesDecoratorViewModelTests
+{
+    [Test]
+    public void OnSortRules_ShouldRaiseSortRulesRequest()
+    {
+        RuleDetailsViewModel rule = new([], null, new());
+        RulePropertiesDecoratorViewModel sut = rule.Properties;
+        EventListener<RulesSortRequest, RuleDetailsViewModel> listener = new();
+        sut.OnSortRules();
+
+        Assert.That(listener.Received, Is.True);
+        Assert.That(listener.Playload, Is.EqualTo(rule));
+    }
+}
