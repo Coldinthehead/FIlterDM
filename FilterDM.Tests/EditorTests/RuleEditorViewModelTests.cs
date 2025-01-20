62afb92df51c65cc02ed46991dda1b38f0ab11f7
coldinthehead <kroters1050595@gmail.com>
2025-01-20 18:12:27 +0300

    add add modifier test

--- FilterDM.Tests/EditorTests/RuleEditorViewModelTests.cs
+++ FilterDM.Tests/EditorTests/RuleEditorViewModelTests.cs
@@ -1,4 +1,25 @@
-﻿namespace FilterDM.Tests.EditorTests;
+﻿using FilterDM.Repositories;
+using FilterDM.Services;
+using FilterDM.ViewModels.EditPage;
+
+namespace FilterDM.Tests.EditorTests;
 public class RuleEditorViewModelTests
 {
+    [Test]
+    public void AddModifierClick_ShouldAddRuleModifier()
+    {
+        RuleDetailsViewModel testModel = WithEmptyModel();
+        RuleEditorViewModel sut = new(testModel);
+        AddModifierViewModel source = sut.AddModifiersList[0];
+
+        source.AddMeCommand.Execute(null);
+
+        Assert.That(testModel.Modifiers, Has.Count.EqualTo(2));
+    }
+
+    public static RuleDetailsViewModel WithEmptyModel()
+    {
+        return new RuleDetailsViewModel(new(), new(new Services.ItemTypeService()), new(new RuleTemplateService(new RuleTemplateRepository())));
+
+    }
 }
