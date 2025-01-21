46d5f8b2b4812b95ccbd5bb957cfad52f3b21eea
coldinthehead <kroters1050595@gmail.com>
2025-01-21 13:15:19 +0300

    add get model rule test

--- FilterDM.Tests/ViewModel.Tests/RuleDetailsViewModelTests.cs
+++ FilterDM.Tests/ViewModel.Tests/RuleDetailsViewModelTests.cs
@@ -1,6 +1,10 @@
 ﻿
+using FilterDM.Models;
+using FilterDM.Repositories;
+using FilterDM.Services;
 using FilterDM.ViewModels.EditPage;
 using FilterDM.ViewModels.EditPage.Events;
+using FilterDM.ViewModels.EditPage.Managers;
 
 namespace FilterDM.Tests.ViewModel.Tests;
 public class RuleDetailsViewModelTests
@@ -14,5 +18,19 @@ public class RuleDetailsViewModelTests
 
         Assert.That(listener.Received, Is.True);
         Assert.That(listener.Playload, Is.EqualTo(sut));
+    }
+
+    [Test]
+    public void GetModel_ShouldSetProperties()
+    {
+        RuleDetailsViewModel sut = new(new(), new(new ItemTypeService()), new RuleTemplateManager(new RuleTemplateService(new RuleTemplateRepository())));
+
+        RuleModel testModel = sut.GetModel();
+
+        Assert.That(testModel, Is.Not.Null);
+        Assert.That(testModel.Title, Is.Not.Null);
+        Assert.That(testModel.Enabled, Is.EqualTo(sut.Properties.Enabled));
+        Assert.That(testModel.Priority, Is.EqualTo(sut.Properties.Priority));
+        Assert.That(testModel.Show, Is.EqualTo(sut.Properties.Show));
     }
 }
