7e4eb0b69940dfd38d28f09d0cd2ad24ed7904bc
coldinthehead <kroters1050595@gmail.com>
2025-01-22 20:50:35 +0300

    add state editor

---
+++ FilterDM/ViewModels/EditPage/ModifierEditors/StateEditorViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FilterDM.ViewModels.Base;
+
+namespace FilterDM.ViewModels.EditPage.ModifierEditors;
+public partial class StateEditorViewModel : ModifierEditorViewModel
+{
+    public StateEditorViewModel(RuleDetailsViewModel rule) : base(rule)
+    {
+    }
+}
